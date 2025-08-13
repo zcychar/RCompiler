32f379c1709bb4794285b2d526b9311e2d00551a
zcychar <zhang.77274@gmail.com>
2025-08-13 23:21:27 +0800

    test:add testcases

---
+++ src/main/resources/array1.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut array: [i32; 3] = [0; 3];
+    array[1] = 1;
+    array[2] = 0;
+}
---
+++ src/main/resources/string2.rs
@@ -0,0 +1,8 @@
+fn main() {
+/*    let a = "123"
+    let b = 'c'
+    let c = c"123"
+*/
+    let d = r#"raw_string\"#
+    let e = cr"c_raw_string\"
+}
