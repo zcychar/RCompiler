7f1a881da76fe5b1ca133a9af6c5e88c3ea6ea3f
zcychar <zhang.77274@gmail.com>
2025-08-14 11:23:18 +0800

    feat:add helper class for parser

---
+++ src/main/kotlin/frontend/AST/TokenContext.kt
@@ -0,0 +1,29 @@
+package frontend.AST
+
+import frontend.Token
+import frontend.TokenType
+import utils.CompileError
+
+class TokenContext(val input: MutableList<Token>) {
+    private var position = 0
+
+
+    fun peek():Token{
+        if(position<input.size)return input[position]
+        throw CompileError("Parser:Out_of_size peek requested")
+    }
+
+    fun consume(){
+        if(position<input.size)position++
+        throw CompileError("Parser:Out_of_size consume requested")
+    }
+
+    fun expect(type: TokenType): String {
+        if(position>=input.size){
+            throw CompileError("Parser:Out_of_size expect requested")
+        }else if(input[position].type!=type){
+            throw CompileError("Parser:Encounter unexpected token ${input[position]}")
+        }
+        return input[position].value
+    }
+}
