b67e10b2ff492a0290ce65f1e58e5e8f15793e6a
zcychar <zhang.77274@gmail.com>
2025-09-12 23:42:55 +0800

    refactor:change structure of semantic part

--- src/main/kotlin/frontend/semantic/Symbol.kt
+++ src/main/kotlin/frontend/semantic/Symbol.kt
@@ -4,14 +4,3 @@ sealed interface Symbol {
     val name: String
     val type: Type
 }
-
-data class StructSymbol(override val name: String, override val type: StructType) : Symbol
-
-data class EnumSymbol(override val name: String, override val type: EnumType) : Symbol
-
-data class FunctionSymbol(override val name: String, override val type: FunctionType) : Symbol
-
-data class ValueSymbol(val mutable: Boolean, override val name: String, override val type: Type, val value: Any?) :
-    Symbol
-
-data class TraitSymbol(override val name: String, override val type: TraitType,val associates:List<Type>) : Symbol
